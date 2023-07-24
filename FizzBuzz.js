d0412ead40fb889873a7ee7565fc8ba75985687c
Mina Lui <minamleung@gmail.com>
2023-07-24 09:51:46 +0100

    change printText to array

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,17 @@
 for (let i = 1; i < 101; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if(i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
+    const printText = [];
+
+    if(i % 3 === 0) {
+        printText.push("Fizz");
     }
+
+    if (i % 5 === 0) {
+        printText.push("Buzz");
+    }
+
+    if (printText.length === 0) {
+        printText.push(i);
+    }
+    
+    console.log(printText.join(""));
 }
